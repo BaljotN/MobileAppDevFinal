f0454d4cf8c24e1b6efc7426ad933436b637acf3
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-12-11 18:38:29 -0700

    Created the footer navigation bar

--- StudyDen/app/src/components/NavigationFooter.jsx
+++ StudyDen/app/src/components/NavigationFooter.jsx
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const NavigationFooter = () => {
   return (
-    <View style={styles.bgIcon}>
-      <Text>NavigationFooter</Text>
+    <View style={styles.footerContainer}>
+      <View style={styles.footerNav}>
+        <Pressable>
+          <Entypo name="home" size={30} color="black" />
+        </Pressable>
+        <Pressable>
+          <Entypo name="calendar" size={24} color="black" />
+        </Pressable>
+        <Pressable>
+          <MaterialIcons name="library-books" size={24} color="black" />
+        </Pressable>
+        <Pressable>
+          <Entypo name="stopwatch" size={24} color="black" />
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -12,7 +27,23 @@ const NavigationFooter = () => {
 export default NavigationFooter;
 
 const styles = StyleSheet.create({
-  bgIcon: {
+  footerContainer: {
+    position: "relative",
+    bottom: 10,
+    width: "100%",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+  },
+  footerNav: {
     backgroundColor: "#DBDBDB",
+    width: 300,
+    height: 70,
+    flexDirection: "row",
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 30,
   },
 });
